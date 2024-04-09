078dbe4e09263b936af6acdc5f869cf044e87f39
Kirill Sharevich <123463231+Checamaro@users.noreply.github.com>
2024-04-09 12:23:31 +0300

    Update test_form.py

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,32 +1,40 @@
-from pages import registration_page
-from pages.registration_page import RegistrationPage
-from data.users import User
+from selene import have
+from selene import browser
+import os
 
 
-def test_fill_form(browser_management):
-    kirill = User(
-        first_name='Kirill',
-        last_name='Sharevich',
-        user_email='email@example.com',
-        user_gender='Male',
-        user_number='9119119191',
-        date='11 April,2024',
-        subject='Maths',
-        hobby='Sports',
-        file='../data/img/pivottable1.png',
-        current_address='Saint-Petersburg',
-        state='Haryana',
-        city='Karnal'
-    )
-
-    page = RegistrationPage()
-
-    page.open()
-    page.fill(kirill)
-    page.submit()
-    page.should_have_registered(kirill)
+"""Заполняем форму регистрации"""
 
 
-
-
-
+def test_form():
+    browser.open('/')
+    browser.element('#firstName').type('Kirill') # input first name
+    browser.element('#lastName').type('Sharevich') # input last name
+    browser.element('#userEmail').type('email@example.com') # input email
+    browser.element('[for="gender-radio-1"]').click() # click gender radio-button
+    browser.element('#userNumber').type('9119119191') # input phone number
+    browser.element('#dateOfBirthInput').click() # click calender
+    browser.element('option[value="2024"]').click() # choose year
+    browser.element('.react-datepicker__day--011').click() # choose date
+    browser.element('#subjectsInput').type('Maths').press_enter() # choose subject from drop-menu
+    browser.element('[for="hobbies-checkbox-1"]').click() # click hobby checkbox
+    browser.element('#uploadPicture').send_keys(os.path.abspath('../data/img/pivottable1.png')) # download picture
+    browser.element('#currentAddress').type('Saint-Petersburg') # typing address
+    browser.element('#react-select-3-input').type('Haryana').press_enter() # choose state from drop-menu
+    browser.element('#react-select-4-input').type('Karnal').press_enter() # choose state from drop-menu
+    browser.element('.practice-form-wrapper [id=submit]').click() # click submit button
+    browser.element('#example-modal-sizes-title-lg').should(
+        have.text('Thanks for submitting the form'))  # check for filled form is visible
+    browser.element('.table').all('tr td:nth-child(2)').should(have.texts
+        (
+        'Kirill Sharevich',
+        'email@example.com',
+        'Male',
+        '9119119191',
+        '11 April,2024',
+        'Maths',
+        'Sports',
+        'pivottable1.png',
+        'Saint-Petersburg',
+        'Haryana Karnal'
+    )) # check for all data
